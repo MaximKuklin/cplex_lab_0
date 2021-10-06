dfb5b6787825ce226183c1bf67064c0437af0e03
Maxim <maksimkakuklin21@gmail.com>
2021-10-07 01:04:02 +0300

    review code

--- cplex/lab_1/lab1.py
+++ cplex/lab_1/lab1.py
@@ -1,9 +1,11 @@
+import argparse
 import os
 import cplex
 from copy import deepcopy
 import itertools
 from igraph import Graph
 import numpy as np
+from time import time
 
 EPS = 1e-5
 
@@ -30,10 +32,8 @@ def get_graph(path):
     return n, m, g
 
 
-def read_files(data_path):
-    paths = sorted([os.path.join(data_path, file) for file in os.listdir(data_path) if "brock" in file])
-    for path in paths[:1]:
-        data = get_graph(path)
+def read_file(data_path):
+    data = get_graph(data_path)
     return data
 
 def get_constraint(ind, names, use_ind=True):
@@ -47,50 +47,63 @@ def get_constraint(ind, names, use_ind=True):
     values[ind[1]] = 1
     return [indexes, values]
 
+if __name__ == '__main__':
 
-n, m, graph = read_files("/old_ssd/media/data/Projects/magistratura/cplex/data/DIMACS_all_ascii")
+    args = argparse.ArgumentParser('Solve lp or ilp problem')
+    args.add_argument('-p', '--path', required=True, help='Path to file DIMACS file')
+    args.add_argument('-t', '--type', required=True, choices=["ILP", "LP"])
+    args = args.parse_args()
 
-names = [f"x{i}" for i in range(0, n)]
-obj = [1]*n
-lower_bounds = [0]*n
-upper_bounds = [1]*n
+    mode = args.type
+    path = args.path
 
-problem = cplex.Cplex()
-problem.objective.set_sense(problem.objective.sense.maximize)
-problem.variables.add(obj=obj,
-                      lb=lower_bounds,
-                      ub=upper_bounds,
-                      names=names,
-                      types=[problem.variables.type.binary]*len(lower_bounds)
-                      )
+    n, m, graph = read_file(path)
 
-for i in range(len(obj)):
-    problem.variables.set_types(i, problem.variables.type.binary)
+    names = [f"x{i}" for i in range(0, n)]
+    if mode == "ILP":
+        obj, lower_bounds, upper_bounds = [1]*n, [0]*n, [1]*n
+    else:
+        obj, lower_bounds, upper_bounds = [1.0]*n, [0.0]*n, [1.0]*n
 
-inverted = graph.complementer().get_edgelist()
+    problem = cplex.Cplex()
+    problem.objective.set_sense(problem.objective.sense.maximize)
+    problem.variables.add(obj=obj,
+                          lb=lower_bounds,
+                          ub=upper_bounds,
+                          names=names,
+                          )
 
-f_e = inverted[0]
-first_constraint = get_constraint(f_e, names, use_ind=False)
-other_constraints = [get_constraint(edge, names) for edge in inverted[1:]]
+    if mode == "ILP":
+        for i in range(len(obj)):
+            problem.variables.set_types(i, problem.variables.type.binary)
 
-constraints = [first_constraint]
-constraints.extend(other_constraints)
+    inverted = graph.complementer().get_edgelist()
 
-rhs = [1]*len(constraints)
+    f_e = inverted[0]
+    first_constraint = get_constraint(f_e, names, use_ind=False)
+    other_constraints = [get_constraint(edge, names) for edge in inverted[1:]]
 
-constraint_senses = ["L"]*len(constraints)
-constraint_names = [f"с{i}" for i in range(0, len(constraints))]
+    constraints = [first_constraint]
+    constraints.extend(other_constraints)
 
-problem.linear_constraints.add(lin_expr = constraints,
-                               senses = constraint_senses,
-                               rhs = rhs,
-                               names = constraint_names)
+    if mode == "ILP":
+        rhs = [1]*len(constraints)
+    else:
+        rhs = [1.0]*len(constraints)
+
+    constraint_senses = ["L"]*len(constraints)
+    constraint_names = [f"с{i}" for i in range(0, len(constraints))]
+
+    problem.linear_constraints.add(lin_expr = constraints,
+                                   senses = constraint_senses,
+                                   rhs = rhs,
+                                   names = constraint_names)
 
 
-problem.set_log_stream(None)
-problem.set_results_stream(None)
-problem.solve()
+    problem.set_log_stream(None)
+    problem.set_results_stream(None)
+    problem.solve()
 
-solution = problem.solution.get_values()
+    solution = problem.solution.get_values()
 
-print(np.argwhere(np.array(solution) > 1-EPS))
+    print(np.argwhere(np.array(solution) > 1-EPS))
